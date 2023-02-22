08a2f46da9d097bcfcb83042ab1b431404f20635
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2023-02-22 15:03:10 +0300

    update function createcommande

--- controllers/commande.controller.js
+++ controllers/commande.controller.js
@@ -2,8 +2,25 @@ const db = require("../models");
 const utilisateur = db.utilisateur;
 const commande = db.commande;
 exports.createCommande = async(req, res) => { // insertion commande d'un utilisateur
-    try {
-      const utilisateurId = req.body.utilisateurId;
+  try {
+    const utilisateurId = req.body.utilisateurId;
+    const idProduct = req.body.idProduct;
+
+    // Vérifier si la commande existe déjà pour ce produit
+    const existingCommande = await commande.findOne({ idProduct: idProduct, utilisateur: utilisateurId });
+
+    if (existingCommande) {
+      // Si la commande existe déjà, augmenter la quantité
+      existingCommande.quantite += req.body.quantite;
+      existingCommande.save((err, updatedCommande) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        res.send({ message: "commande was updated successfully!" });
+      });
+    } else {
+      // Si la commande n'existe pas encore, la créer
       utilisateur.findOne({ _id: utilisateurId }, (err, utilisateur) => {
         if (err) {
           res.status(500).send({ message: err });
@@ -14,9 +31,10 @@ exports.createCommande = async(req, res) => { // insertion commande d'un utilisa
           return;
         }
         const commandes = new commande({
-            idProduct: req.body.idProduct,
+            idProduct: idProduct,
             name : req.body.name,
             color:req.body.color,
+            quantite:req.body.quantite,
             price:req.body.price,
             status:req.body.status,
             utilisateur: utilisateurId
@@ -29,7 +47,8 @@ exports.createCommande = async(req, res) => { // insertion commande d'un utilisa
           res.send({ message: "commande was saved successfully!" });
         });
       });
-    } catch (error) {
-      res.status(500).send({ message: error.message });
     }
-  };
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
--- models/commande.model.js
+++ models/commande.model.js
@@ -6,6 +6,7 @@ const Commande = mongoose.model(
       idProduct: {type:String},
       name : {type:String},
       color:{type: String},
+      quantite:{type:Number},
       price:{type: mongoose.Types.Decimal128},
       status:{type:Boolean,default:false},
       utilisateur: { type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateur' }
